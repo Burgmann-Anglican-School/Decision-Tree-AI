af3e07bd1e50b6c80ea04f5a9a969a39bc671c4e
Anpazkajan <a.arulmugavarathan@stu.burgmann.act.edu.au>
2025-05-19 22:47:41 +1000

    Redo to determine how different hands will be triggered, and shrinking the prior clutter for the different cards to be added

--- ai.py
+++ ai.py
@@ -1,22 +1,43 @@
-initial_hand_dealer = []
-initial_hand_you = []
-
 d1, d2 = str(input('Dealer hand:')).split()
 
 y1, y2 = str(input('Your hand:')).split()
 
+t = game(d1, d2, y1, y2)
 
-def hand_determine(val1, val2, new_val):
-    if new_val == None:
-        if val1 == 'Ace' and val2 == 'Ace':
-            total_possibility = [12, 2]
-            return total_possibility
-        if val1 == 'Ace' and val2 != 'Ace':
-            if val2 == 'king' or val2 == 'jack' or val2 == 'queen' or val2 == '10': 
-                total_possibility = [21]
-                return total_possibility
-        if val2 == 'Ace' and val1 != 'Ace':
-            if val1 == 'king' or val1 == 'jack' or val1 == 'queen' or val1 == '10': 
-                total_possibility = [21]
-                return total_possibility
-            
+def game(d1, d2, y1, y2):
+    cards = Deck()
+    dealer = [d1, d2]
+    player = [y1, y2]
+    
+    total = calculations(player)
+    return total
+
+def calculations(hand):
+    total = 0
+    ace = 0
+    for card in hand:
+        try:
+            total += int(card)
+        except:
+            if card != 'Ace':
+                total += 10
+            elif card == 'Ace':
+                if total > 11:
+                    total += 11
+                    ace += 1
+                else:
+                    total += 1
+    while total > 21 and ace > 0:
+        total -= 10
+    return total
+
+
+class Deck():
+    def __init__(self):
+        suits = ['Heart', 'Diamond', 
+                 'Club', 'Spade']
+        rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        self.all_cards = []
+        for i in suits:
+            for a in rank:
+                self.all_cards.append((i,a))
