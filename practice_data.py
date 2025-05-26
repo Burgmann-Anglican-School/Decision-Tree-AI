ecfdc1ab0d34ea14931b5bbc9c0af0c6d4207ff4
Anpazkajan <a.arulmugavarathan@stu.burgmann.act.edu.au>
2025-05-26 22:02:01 +1000

    Design and visualisation of data including deciding new cards

--- practice_data.py
+++ practice_data.py
@@ -1,6 +1,7 @@
 from sklearn.tree import DecisionTreeClassifier
 import pandas as panda
 import Data_Sets
+import matplotlib.pyplot as plots
 
 class DecisionTree:
     def __init__(self):
@@ -8,11 +9,15 @@ class DecisionTree:
         self.visual = None
         self.criterion = 'entropy'
         self.max_depth = 4
+
     def train(self):
         x_coordinate = self.visual['you_total']
         y_coordinate = self.visual['future_choices']
         self.model = DecisionTreeClassifier(criterion='entropy', max_depth=self.max_depth)
         self.model.fit(x_coordinate, y_coordinate)
+    
+    def guess_new_card(self, risk, you, dealer):
+        return self.model.predict([[you, dealer, risk]])
 
     def decisions(self):
         for i in range(len(Data_Sets.data['future_choices'])):
@@ -31,3 +36,9 @@ class DecisionTree:
             else:
                 Data_Sets.data_sets['Future_choices'].append('hit')
         self.visual= panda.DataFrame(Data_Sets.data_sets)
+
+    def visual_demonstration(self):
+        plots.figure(figsize=(0, 21))
+        plots.plot_tree(self.model, values = ['total_you', 'dealer_initial', 'risk'], names=self.model.classes, filled=True)
+        plots.show() 
+
