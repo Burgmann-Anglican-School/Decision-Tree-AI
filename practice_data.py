bb045cb5de54dad578bd9e8fe27c17ef8aea4c2e
Anpazkajan <a.arulmugavarathan@stu.burgmann.act.edu.au>
2025-05-26 21:34:51 +1000

    Training and visualization of the data from the user

---
+++ practice_data.py
@@ -0,0 +1,33 @@
+from sklearn.tree import DecisionTreeClassifier
+import pandas as panda
+import Data_Sets
+
+class DecisionTree:
+    def __init__(self):
+        self.model = None
+        self.visual = None
+        self.criterion = 'entropy'
+        self.max_depth = 4
+    def train(self):
+        x_coordinate = self.visual['you_total']
+        y_coordinate = self.visual['future_choices']
+        self.model = DecisionTreeClassifier(criterion='entropy', max_depth=self.max_depth)
+        self.model.fit(x_coordinate, y_coordinate)
+
+    def decisions(self):
+        for i in range(len(Data_Sets.data['future_choices'])):
+            risk = Data_Sets.data['risk'][i]
+            total_you = Data_Sets.data['you_total'][i] 
+            dealer_inital = Data_Sets.data['dealer'][i]
+
+            if total_you >= 18: 
+               Data_Sets.data_sets['Future_choices'].append('stand')
+            if total_you <= 11:
+                Data_Sets.data_sets['Future_choices'].append('hit')
+            if 18 >= total_you >= 11 and dealer_inital >= 7:
+                Data_Sets.data_sets['Future_choices'].append('hit')
+            if 18 >= total_you >= 11 and dealer_inital <= 7:
+                Data_Sets.data_sets['Future_choices'].append('stand')
+            else:
+                Data_Sets.data_sets['Future_choices'].append('hit')
+        self.visual= panda.DataFrame(Data_Sets.data_sets)
