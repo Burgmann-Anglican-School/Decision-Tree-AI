78781b8962651e88d1fe6c57df28394364988ba5
Anpazkajan <a.arulmugavarathan@stu.burgmann.act.edu.au>
2025-05-28 13:51:13 +1000

    Inclusion of reset mechanic

--- practice_data.py
+++ practice_data.py
@@ -13,7 +13,6 @@ class DecisionTree:
         list_dealer = list(range(2, 12))*4
         list_dealer.append(2)
         list_dealer.append(3)
-        print(len(list_dealer))
         data_sets = {'you_total':list(range(8,22))*3, 'dealer_initial': list_dealer, 'risk':(list(range(0, 3)))*14, 'future_choices':[]}
         for i in range(len(data_sets['you_total'])):
             you_val = data_sets['you_total'][i]
@@ -26,7 +25,6 @@ class DecisionTree:
                 data_sets['future_choices'].append('hit')
             else:
                 data_sets['future_choices'].append('stand')
-        print(data_sets['future_choices'])
         self.data_sets = data_sets
     def train(self):
         x_coordinate = self.visual[['you_total', 'dealer_initial', 'risk']]
@@ -50,6 +48,16 @@ class DecisionTree:
             decide = self.guess_new_card(i['risk'], i['you_total'], i['dealer_initial'])
             print(decide)
 
+def reset():
+    while True:
+        play_again = str(input('Would you like to stop playing? (y/n) ')).lower()
+        if play_again == 'y':
+            return True
+        if play_again == 'n':
+            return False
+        else:
+            print('Invalid input')
+        
 
 
 
@@ -57,7 +65,35 @@ modelling = DecisionTree()
 modelling.decisions()
 modelling.train()
 modelling.visual_demonstration()
-your_total = int(input('Your total:'))
+
+aces = 0
+your_total = int(input('Your initial total(Consider, K, Q, J as 10, and A as 11): '))
 dealer_initial = int(input('Dealer total: '))
 risk = int(input('Risk: '))
 print(modelling.case_tests([{'you_total': your_total, 'dealer_initial': dealer_initial, 'risk': risk}]))
+tf = True
+while tf:
+    add_total = int(input('Your new card(Consider K, Q, J as 10): '))
+    your_total += add_total
+    if add_total == 11:
+        aces += 1
+    if your_total > 21:
+        if aces > 0:
+            aces -= 1
+            your_total -= 10
+        else:
+            print('You lost')
+            break
+    if your_total == 21:
+        print('You won')
+        
+    risk = int(input('Risk: '))
+    print(modelling.case_tests([{'you_total': your_total, 'dealer_initial': dealer_initial, 'risk': risk}]))
+    dealer = str(input('Dealer stand or hit? '))
+    if dealer.lower() == 'stand':
+        dealer_final = int(input('What is the dealers final total: '))
+    if dealer_final < your_total:
+        print('You won')
+    else:
+        print('You lost')
+    tf = reset()
